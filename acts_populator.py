4e178284965c7e0b752eae866604e35dc4543e7f
Douglas Dolitsky <Dolitsky_Doug@MacBook-Pro-4.home>
2015-11-27 15:13:04 -0500

    added acts populator

--- acts_populator.py
+++ acts_populator.py
@@ -17,12 +17,10 @@ list_movies = file_data.split('\n')
 index = 0
 
 # initializing values
+actor_id = 0
 movie_id = 0
-title = ""
-revenue = 0
-budget = 0
-adult = 0
-user_rating = 0.0
+
+cast = []
 
 connection_ub = connection.MySQLConnection (user='user', password='12345678', host='mysql-instance.ctyplruqgzr3.us-east-1.rds.amazonaws.com', database='mydb')
 cursor_ub = connection_ub.cursor()
@@ -33,24 +31,19 @@ for tuple_ in list_movies :
     tuple_ = tuple_.encode('ascii','ignore')
     movie_data = json.loads(tuple_)
     index = index + 1
-    if (index % 500 == 0) :
+    if (index % 100 == 0) :
         print "===== In Index " + str(index) + "=================="
     for key in movie_data.keys() :
         if (key == "id") :
             movie_id = movie_data[key]
-        if (key == "title") :
-            title = movie_data[key].strip()
-        if (key == "revenue") :
-            revenue = movie_data[key]
-        if (key == "budget") :
-            budget = movie_data[key]
-        if (key == "adult") :
-            adult = movie_data[key]
-        if (key == "userrating") :
-            user_rating = movie_data[key]
-    
-    sql_insert = ("INSERT INTO movies(movie_id, title, revenue, budget, adult, user_rating) VALUES (%s,%s,%s,%s,%s,%s)")
-    cursor_ub.execute(sql_insert, (movie_id, title, revenue, budget, adult, user_rating))
+        if (key == "cast") :
+            cast = movie_data[key]
+    for actor in cast :
+        for actor_key in actor.keys() :
+            if (key == "personId") :
+                actor_id = actor[key]
+            sql_insert = ("INSERT INTO acts(actor_id, movie_id) VALUES (%s,%s)")
+            cursor_ub.execute(sql_insert, (actor_id, movie_id))
     
 connection_ub.commit()
 connection_ub.close()
